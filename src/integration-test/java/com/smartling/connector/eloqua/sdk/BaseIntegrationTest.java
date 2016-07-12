ae8489d22cfb8fd3dc609297a3940c03fde96531
Dmitry Lyash <dlyash@smartling.com>
2016-07-12 11:29:35 -0400

    ELQ-6: Ignoring integration tests if site name / username / password are missing

--- src/integration-test/java/com/smartling/connector/eloqua/sdk/BaseIntegrationTest.java
+++ src/integration-test/java/com/smartling/connector/eloqua/sdk/BaseIntegrationTest.java
@@ -3,19 +3,36 @@ package com.smartling.connector.eloqua.sdk;
 import com.smartling.connector.eloqua.sdk.client.EmailEloquaClient;
 import com.smartling.connector.eloqua.sdk.rest.model.Elements;
 import com.smartling.connector.eloqua.sdk.rest.model.Email;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeNotNull;
 
 public class BaseIntegrationTest
 {
+    private Configuration configuration;
+
+    @Before
+    public void setUp() throws Exception {
+        String siteName = System.getProperty("eloqua.siteName");
+        String username = System.getProperty("eloqua.username");
+        String password = System.getProperty("eloqua.password");
+
+        assumeNotNull("Site name is not specified", siteName);
+        assumeNotNull("Username is not specified", username);
+        assumeNotNull("Password is not specified", password);
+
+        this.configuration = new Configuration(siteName, username, password);
+    }
+
     @Test
     public void testCall()
     {
-        Configuration configuration = new Configuration(System.getProperty("eloqua.siteName"), System.getProperty("eloqua.username"), System.getProperty("eloqua.password"));
-
         EmailEloquaClient emailEloquaClient = new EmailEloquaClient(configuration);
+
         Elements<Email> emails = emailEloquaClient.listEmails();
+
         assertEquals(2, emails.total.intValue());
     }
 }
