eeb281848b38ee6c704078417133ebdf5be4ed37
Dmitry Lyash <dlyash@smartling.com>
2016-07-12 13:12:31 -0400

    ELQ-6: Fix code style

--- src/test/java/com/smartling/connector/eloqua/sdk/ConfigurationTest.java
+++ src/test/java/com/smartling/connector/eloqua/sdk/ConfigurationTest.java
@@ -18,27 +18,32 @@ public class ConfigurationTest
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldValidateSiteName() throws Exception {
+    public void shouldValidateSiteName() throws Exception
+    {
         new Configuration(null, "username", "password");
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldValidateUsername() throws Exception {
+    public void shouldValidateUsername() throws Exception
+    {
         new Configuration("site", null, "password");
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldValidatePassword() throws Exception {
+    public void shouldValidatePassword() throws Exception
+    {
         new Configuration("site", "username", null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldValidateConnectionTimeout() throws Exception {
+    public void shouldValidateConnectionTimeout() throws Exception
+    {
         configuration.setConnectTimeoutMillis(-1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldValidateReadTimeout() throws Exception {
+    public void shouldValidateReadTimeout() throws Exception
+    {
         configuration.setReadTimeoutMillis(-1);
     }
 
